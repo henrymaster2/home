96f1eda23fe8e490782c06a4a7c4a7c03eab29ff
Henry Master <masterhenry681@gmail.com>
2026-08-09 17:25:32 +0300

    pwa test

--- lib/home_web/live/admin/notifications_live.ex
+++ lib/home_web/live/admin/notifications_live.ex
@@ -8,8 +8,16 @@ defmodule HomeWeb.Admin.Notifications do
   def render(assigns) do
     ~H"""
     <div>
-      notifications Page
-    </div>
+  <h1>Notification Page</h1>
+
+  <button
+    id="install-pwa"
+    hidden
+    class="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
+  >
+    Install Home App
+  </button>
+</div>
     """
   end
 end
